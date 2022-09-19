9e07427b7747067e44a268d7132ad1f49bfc4ffd
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-19 18:14:09 -0300

    react icon test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Review from "./Review";
+import { GiAbstract079 } from "react-icons/gi";
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <h2>
+      reviews project setup
+      <GiAbstract079 />
+    </h2>
+  );
 }
 
 export default App;
